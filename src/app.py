8473628f937c3560242cb75d9f5f356c07cb296a
akshayputtaswamy23 <akshayputtaswamy23@gmail.com>
2026-09-08 02:03:25 +0530

    Trigger git actions using self hosted runners

--- src/app.py
+++ src/app.py
@@ -12,8 +12,9 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3',
+        'message': 'You are doing great, little human! <3, 123 Vishnuvardhana na , Karunada kingu nane na',
         'deployed_on': 'kubernetes'
+
     })
 
 @app.route('/api/v1/healthz')
